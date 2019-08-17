071f855d912b4c480d05025b7ef200fc213d5c25
eralpkor <eralp.ny@gmail.com>
2019-08-17 14:49:30 -0400

    added pagination

--- starwars/src/components/Button/Btn.js
+++ starwars/src/components/Button/Btn.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import { Container } from "semantic-ui-react";
 
 function Btn(props) {
   return (
-    <Container textAlign="center">
-      <Button color="orange">Next</Button>
+    <Container>
+      <Segment inverted>
+        <h1 className="Header">React Wars</h1>
+        <div>
+          {props.page > 1 && (
+            <Button inverted onClick={() => props.prevPage()} color="purple">
+              Previous
+            </Button>
+          )}
+          <p>Page: {props.page}</p>
+          <Button inverted onClick={() => props.nextPage()} color="orange">
+            Next
+          </Button>
+        </div>
+      </Segment>
     </Container>
   );
 }
